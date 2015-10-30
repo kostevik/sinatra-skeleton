ea69f63628e4a69e44b3283d49fb9f3287ff690d
kostevik <kariostevik@gmail.com>
2015-10-29 19:56:14 -0700

    Change Config variable on database.rb

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,6 @@
 configure :development, :production do
 
-	db = URI.parse('postgres:///testdatabase')
+	db = URI.parse('postgres://pprikjwzgmpjfx:zLoT3uS7-ZVoOmdVEedEGTfysE@ec2-107-21-219-142.compute-1.amazonaws.com:5432/dchk160detf376')
 
 	ActiveRecord::Base.establish_connection(
 	  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
